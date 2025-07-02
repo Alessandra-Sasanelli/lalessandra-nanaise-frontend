6f7f26bdf536013230e6e0591736aaa048533b79
alessandrasasanelli <a.sasanelli39@gmail.com>
2025-07-02 22:14:57 +0200

    Added first components (header and dropdown for header)

---
+++ src/components/DropdownMenu.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "DropdownMenu",
+  props: {
+    dropdownName: {
+      type: String,
+      required: true
+    },
+    linkObjects: {
+      type: Array,
+      required: true
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="dropdown">
+    <button class="dropbtn">{{ dropdownName }}</button>
+    <div class="dropdown-content">
+      <a :href="linkObject.linkValue" v-for="linkObject in linkObjects">linkObject.linkName</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import DropdownMenu from './DropdownMenu.vue';
+
+export default defineComponent({
+  name: "Header",
+  components: {
+    DropdownMenu
+  }
+})
+
+const eventsName = Events
+const linkObjects = [
+  {
+    linkName: "Past events",
+    linkValue: "/past_events",
+  },
+  {
+    linkName: "Upcoming events",
+    linkValue: "/upcoming_events",
+  },
+  {
+    linkName: "Gallery",
+    linkValue: "/gallery",
+  },
+]
+</script>
+
+<template>
+<div>
+  <div>
+    <h1></h1>
+    <span><a href="/">Home</a></span>
+    <DropdownMenu :dropdown-name=eventsName link-objects=""></DropdownMenu>
+    <DropdownMenu></DropdownMenu>
+    <DropdownMenu></DropdownMenu>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
